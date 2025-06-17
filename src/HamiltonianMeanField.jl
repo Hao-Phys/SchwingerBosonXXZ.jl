c5c9f3b03c66b03984f03c0eff665e042f611f41
Hao Zhang <hzhangphys@gmail.com>
2025-06-17 18:50:01 -0400

    Small tweaks

--- src/HamiltonianMeanField.jl
+++ src/HamiltonianMeanField.jl
@@ -40,18 +40,19 @@ function dynamical_matrix!(D::Matrix{ComplexF64}, sbs::SchwingerBosonSystem, q_r
 
             # Below we follow the convention in Sunny to define the dynamical matrix
             # D11 and D22
-            D11[i, j] += Q_link(α, sign, J₊, J₋, Bs, Cs) * link_phase(α, q_reshaped)
-            D11[j, i] += conj(Q_link(α, sign, J₊, J₋, Bs, Cs) * conj(link_phase(α, q_reshaped)))
-            D22[i, j] += conj(Q_link(α, sign, J₊, J₋, Bs, Cs) * link_phase(α, q_reshaped))
-            D22[j, i] += Q_link(α, sign, J₊, J₋, Bs, Cs) * conj(link_phase(α, q_reshaped))
+            phase = link_phase(σ, q_reshaped)
+            D11[i, j] += Q_link(α, sign, J₊, J₋, Bs, Cs) * phase
+            D11[j, i] += conj(Q_link(α, sign, J₊, J₋, Bs, Cs) * conj(phase))
+            D22[i, j] += conj(Q_link(α, sign, J₊, J₋, Bs, Cs) * phase)
+            D22[j, i] += Q_link(α, sign, J₊, J₋, Bs, Cs) * conj(phase)
 
             # D21 and D12
             increment = σ == 1 ? 3 : 1
             j = mod1(i+increment, 6)
-            D21[i, j] += P_link(α, sign, J₊, J₋, As, Ds) * link_phase(α, q_reshaped)
-            D21[j, i] += P_link(α, sign, J₊, J₋, As, Ds) * conj(link_phase(α, q_reshaped))
-            D12[i, j] += conj(P_link(α, sign, J₊, J₋, As, Ds) * link_phase(α, q_reshaped))
-            D12[j, i] += conj(P_link(α, sign, J₊, J₋, As, Ds) * conj(link_phase(α, q_reshaped)))
+            D21[i, j] += P_link(α, sign, J₊, J₋, As, Ds) * phase
+            D21[j, i] += P_link(α, sign, J₊, J₋, As, Ds) * conj(phase)
+            D12[i, j] += conj(P_link(α, sign, J₊, J₋, As, Ds) * phase)
+            D12[j, i] += conj(P_link(α, sign, J₊, J₋, As, Ds) * conj(phase))
 
             # Diagonal terms
             D11[i, i] += λs[α]
