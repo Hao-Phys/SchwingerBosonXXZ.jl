f36c97e62bcd5107f0226009763c7c4de72fb278
Hao-Phys <hzhangphys@gmail.com>
2025-09-08 18:27:52 -0400

    Commit before the implementation of the aux chemical potential method

--- src/ObjectiveFunctions.jl
+++ src/ObjectiveFunctions.jl
@@ -75,7 +75,7 @@ function divided_aux!(tmp, tmp2, Dmat, ∂D∂X, V, inv_V)
     mul!(tmp, tmp2, inv_V)
 end
 
-# The variational free energy objective function for Optim
+# The variational free energy objective function with (incorrect gradient) for Optim
 function fg_ϕ!(sbs::SchwingerBosonSystem, f, g, ϕ)
     if isnothing(g)
         g = zero(ϕ)
@@ -204,6 +204,129 @@ function fg_ϕ!(sbs::SchwingerBosonSystem, f, g, ϕ)
                     g[α] -= g2[β]
                 end
             end
+        end
+
+        μs = sbs.mean_fields[13:15]
+        for α in 1:3
+            f += real(μs[α]) * (2S+1)
+        end
+        return f
+    else
+        @warn "The number of Schwinger bosons per site is not equal to 2S, returning Inf. Skipping this configuration..."
+        g .= Inf
+        f = Inf
+        return f
+    end
+
+end
+
+# The variational free energy objective function for Optim
+function fϕ!(sbs::SchwingerBosonSystem, ϕ)
+    # Make sure to set the mean fields to the `sbs` object
+    set_ϕ!(sbs, ϕ)
+
+    # Calculate the "temperature*entropy" term of the free energy
+    (; L, J, Δ, S, T) = sbs
+    Nu = L^2
+
+    # Buffers
+    D = zeros(ComplexF64, 12, 12)
+    V = zeros(ComplexF64, 12, 12)
+    g = zeros(24)
+
+    # Maximize the mean-field free energy to find the optimal chemical potential
+    # But we need a μ0 such that the dynamical matrix is positive definite
+    eigvals_min = Float64[]
+    for i in 1:L, j in 1:L
+        q = Vec3([(i-1)/L, (j-1)/L, 0.0])
+        dynamical_matrix!(D, sbs, q)
+        eigval_min = eigmin(D)
+        push!(eigvals_min, eigval_min)
+    end
+
+    τ = max(0.0, -minimum(eigvals_min))
+    μ0s = sbs.mean_fields[13:15] .- (τ + T)
+
+    optimize_μ!(sbs, μ0s)
+
+    f = 0.0
+    nα = zeros(3)
+
+    # Contribution from the "entropy" term
+    for i in 1:L, j in 1:L
+        q = Vec3([(i-1)/L, (j-1)/L, 0.0])
+        dynamical_matrix!(D, sbs, q)
+        try
+            E = bogoliubov!(V, D)
+            for n in 1:6
+                f += E[n] / (2Nu)
+                (T > 1e-8) && (f += real(T * log1p(-exp(-E[n]/T))) / Nu)
+                for α in 1:3
+                    nα[α] += ((abs2(V[combine_index(α,1), n]) + abs2(V[combine_index(α,2), n])) * bose(E[n], T) + (abs2(V[combine_index(α,1), n+6]) + abs2(V[combine_index(α,2), n+6]) * bose(E[n+6], T))) / Nu
+                end
+            end
+        # If the dynamical matrix is not positive definite, return to Inf immediately
+        catch _
+            @warn "Dynamical matrix is not positive definite, returning Inf. Skipping this configuration..."
+            g .= Inf
+            f = Inf
+            return f
+        end
+    end
+
+    # Sometimes even though the dynamical matrix is positive definite, the number of Schwinger bosons per site may different from 2S, leading to unphysical mean-fields.
+    if prod(isapprox.(nα, 2S; atol=1e-4))
+        # The "normal" operation
+        # Buffers to calculate the gradient
+        P = zeros(ComplexF64, 12, 12)
+        tmp = zeros(ComplexF64, 12, 12)
+        # Buffers to hold the derivatives of the dynamical matrix
+        ∂ID∂ϕs = zeros(ComplexF64, 12, 12, 24)
+
+        # The "interaction" strength
+        J₊ = J * (Δ + 1) / 2
+        J₋ = J * (Δ - 1) / 2
+        inv_J₊ = J₊ == 0 ? 0 : 1 / J₊
+        inv_J₋ = J₋ == 0 ? 0 : 1 / J₋
+        fα = zeros(24)
+        inv_fα = zeros(24)
+        for α in (1, 2, 3, 13, 14, 15)
+            fα[α] = -J₊
+            inv_fα[α] = -inv_J₊
+        end
+        for α in (4, 5, 6, 16, 17, 18)
+            fα[α] = J₊
+            inv_fα[α] = inv_J₊
+        end
+        for α in (7, 8, 9, 19, 20, 21)
+            fα[α] = J₋
+            inv_fα[α] = inv_J₋
+        end
+        for α in (10, 11, 12, 22, 23, 24)
+            fα[α] = -J₋
+            inv_fα[α] = -inv_J₋
+        end
+
+        # Here we calculate the gradient contribution from the entropy term and the Πα vector and ∂Παβ matrix
+        for i in 1:L, j in 1:L
+            q = Vec3([(i-1)/L, (j-1)/L, 0.0])
+            single_particle_density_matrix!(P, D, V, tmp, sbs, q)
+
+            @views for α in 1:3
+                ∂ID∂A!(∂ID∂ϕs[:, :, α], ∂ID∂ϕs[:, :, α+12], tmp, sbs, q, α)
+                ∂ID∂B!(∂ID∂ϕs[:, :, α+3], ∂ID∂ϕs[:, :, α+15], tmp, sbs, q, α)
+                ∂ID∂C!(∂ID∂ϕs[:, :, α+6], ∂ID∂ϕs[:, :, α+18], tmp, sbs, q, α)
+                ∂ID∂D!(∂ID∂ϕs[:, :, α+9], ∂ID∂ϕs[:, :, α+21], tmp, sbs, q, α)
+            end
+
+            @views for α in 1:24
+                g[α] += real(tr(P * ∂ID∂ϕs[:, :, α])) / Nu
+            end
+        end
+
+        # Now we add the contribution from the "correction" term L"⟨H - H_{MF}⟩_{MF}"
+        for α in 1:24
+            f += inv_fα[α] * g[α]^2 / 12 - g[α] * ϕ[α]
         end
 
         μs = sbs.mean_fields[13:15]
