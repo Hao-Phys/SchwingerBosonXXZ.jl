62f9b0a3166fd2775e0b7be88e319d1cafee0a06
Hao-Phys <hzhangphys@gmail.com>
2025-09-17 12:52:50 -0400

    Corrected the warning information of SCF solver

--- src/SelfConsistentEqns.jl
+++ src/SelfConsistentEqns.jl
@@ -25,7 +25,7 @@ end
 function solve_self_consistent_mean_fields!(sbs::SchwingerBosonSystem, x0::Vector{ComplexF64}; opts...)
     sce_eqn! = (f, x) -> self_consistent_mean_fields!(f, x, sbs)
     ret = nlsolve(sce_eqn!, x0; opts...)
-    !converged(ret) && @warn "Self-consitent equations converged to a solution with residual $(ret.residual)"
+    !converged(ret) && @warn "Self-consitent equations converged to a solution with residual $(ret.residual_norm)"
     best_mean_fields = [ret.zero; sbs.mean_fields[13:15]]
     set_mean_fields!(sbs, best_mean_fields)
 end
