3096bebca73cd511bf60f0a1a140ad48f33bb505
Hao-Phys <hzhangphys@gmail.com>
2026-05-26 21:06:29 -0400

    Refactor the code structure: move canonical mean-field related files to the dedicated folder

--- src/SchwingerBosonXXZ.jl
+++ src/SchwingerBosonXXZ.jl
@@ -9,28 +9,28 @@ using HCubature
 include("Types.jl")
 export SchwingerBosonSystem, set_mean_fields!, set_μ0!, set_ϕ!, set_classical_mean_fields!
 
-include("FiniteSizeTools.jl")
+include("CanonicalMeanField/FiniteSizeTools.jl")
 export q_space_path_sbs
 
-include("HamiltonianMeanField.jl")
-include("DispersionAndIntensities.jl")
+include("CanonicalMeanField/HamiltonianMeanField.jl")
+include("CanonicalMeanField/DispersionAndIntensities.jl")
 export excitations, dispersion, dssf_mean_field
-include("GradientofHamiltonian.jl")
+include("CanonicalMeanField/GradientofHamiltonian.jl")
 
-include("KuboMori.jl")
-include("ObjectiveFunctions.jl")
+include("CanonicalMeanField/KuboMori.jl")
+include("CanonicalMeanField/ObjectiveFunctions.jl")
 export variational_free_energy
 
-include("ExpectationValues.jl")
+include("CanonicalMeanField/ExpectationValues.jl")
 export expectation_values
 
-include("Optimization.jl")
+include("CanonicalMeanField/Optimization.jl")
 export optimize_mean_fields!, optimize_μ0!
 
 # For legacy code that we want to keep for reference but not export.
-include("Legacy.jl")
+include("CanonicalMeanField/Legacy.jl")
 
-include("SelfConsistentEqns.jl")
+include("CanonicalMeanField/SelfConsistentEqns.jl")
 export solve_self_consistent_mean_fields_condensed!
 
 end
